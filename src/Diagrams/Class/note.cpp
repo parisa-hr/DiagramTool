b2eb919de0cc845b0a7daa08b29a9169b8d7bc1c
Parisa Hamed <parisahamed76@gmail.com>
2021-07-12 23:06:49 +0430

    add notItem

--- src/Diagrams/Class/note.cpp
+++ src/Diagrams/Class/note.cpp
@@ -1,6 +1,40 @@
 #include "note.h"
 
+#include <QPainter>
+#include <QPen>
+
 Note::Note()
 {
+    setAcceptHoverEvents(true);
+    setZValue(101);
+}
 
+QRectF  Note::boundingRect() const
+{
+    return _rect;
+}
+
+QPainterPath  Note::shape() const
+{
+    QPainterPath  path;
+
+    path.addRect(_rect);
+
+    return path;
+}
+
+void  Note::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget)
+{
+    Q_UNUSED(widget);
+
+    auto  pen = QPen(Qt::black, 2);
+    pen.setCosmetic(true);
+    painter->setBrush(Qt::NoBrush);
+    painter->setRenderHint(QPainter::Antialiasing);
+    painter->setPen(pen);
+}
+
+void  Note::setRect(const QRectF &rect)
+{
+    _rect = rect;
 }
--- src/Diagrams/Class/note.h
+++ src/Diagrams/Class/note.h
@@ -1,11 +1,23 @@
 #ifndef NOTE_H
 #define NOTE_H
 
+#include <QGraphicsItem>
 
-class Note
+class Note: public QGraphicsItem
 {
 public:
     Note();
+
+    QRectF        boundingRect() const override;
+
+    QPainterPath  shape() const override;
+
+    void          paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget) override;
+
+    void          setRect(const QRectF &rect);
+
+private:
+    QRectF  _rect = QRectF(0, 0, 200, 100);
 };
 
 #endif // NOTE_H
