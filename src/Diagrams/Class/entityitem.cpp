0da010ccd003e41881c26a6e9359e7d909b5e6ed
Parisa Hamed <parisahamed76@gmail.com>
2021-07-12 23:06:26 +0430

    add EntityItem

--- src/Diagrams/Class/entityitem.cpp
+++ src/Diagrams/Class/entityitem.cpp
@@ -1,6 +1,43 @@
 #include "entityitem.h"
 
-EntityItem::EntityItem(QObject *parent) : QObject(parent)
-{
+#include <QPainter>
+#include <QPen>
 
+EntityItem::EntityItem(QObject *parent)
+{
+    setAcceptHoverEvents(true);
+    setZValue(101);
+}
+
+QRectF  EntityItem::boundingRect() const
+{
+    return _rect;
+}
+
+QPainterPath  EntityItem::shape() const
+{
+    QPainterPath  path;
+
+    path.addRect(_rect);
+
+    return path;
+}
+
+void  EntityItem::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget)
+{
+    Q_UNUSED(widget);
+
+    auto  pen = QPen(Qt::black, 2);
+    pen.setCosmetic(true);
+    painter->setBrush(Qt::NoBrush);
+    painter->setRenderHint(QPainter::Antialiasing);
+    painter->setPen(pen);
+
+    painter->drawLine(0, _rect.height(), _rect.width(), _rect.height());
+    painter->drawEllipse(0.0, 0.0, _rect.width(), _rect.height());
+}
+
+void  EntityItem::setRect(const QRectF &rect)
+{
+    _rect = rect;
 }
--- src/Diagrams/Class/entityitem.h
+++ src/Diagrams/Class/entityitem.h
@@ -2,15 +2,24 @@
 #define ENTITYITEM_H
 
 #include <QObject>
+#include <QGraphicsItem>
 
-class EntityItem : public QObject
+
+class EntityItem: public QGraphicsItem
 {
-    Q_OBJECT
 public:
     explicit EntityItem(QObject *parent = nullptr);
 
-signals:
+    QRectF        boundingRect() const override;
 
+    QPainterPath  shape() const override;
+
+    void          paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget) override;
+
+    void          setRect(const QRectF &rect);
+
+private:
+    QRectF  _rect = QRectF(0, 0, 100, 100);
 };
 
 #endif // ENTITYITEM_H
