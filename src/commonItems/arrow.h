6361f2f17dd419d8f49a1c23cf643914cef2230d
Parisa Hamed <parisahamed76@gmail.com>
2021-07-18 23:26:25 +0430

    add arrow class

---
+++ src/commonItems/arrow.h
@@ -0,0 +1,59 @@
+#ifndef ARROW_H
+#define ARROW_H
+
+#include <QGraphicsLineItem>
+
+class DiagramItem;
+
+//! [0]
+class Arrow: public QGraphicsLineItem
+{
+public:
+    enum
+    {
+        Type = UserType + 4
+    };
+
+    Arrow(DiagramItem *startItem, DiagramItem *endItem,
+          QGraphicsItem *parent = nullptr);
+
+    int  type() const override
+    {
+        return Type;
+    }
+
+    QRectF        boundingRect() const override;
+
+    QPainterPath  shape() const override;
+
+    void          setColor(const QColor &color)
+    {
+        myColor = color;
+    }
+
+    DiagramItem* startItem() const
+    {
+        return myStartItem;
+    }
+
+    DiagramItem* endItem() const
+    {
+        return myEndItem;
+    }
+
+    void  updatePosition();
+
+protected:
+    void  paint(QPainter *painter, const QStyleOptionGraphicsItem *option,
+                QWidget *widget = nullptr) override;
+
+private:
+    DiagramItem *myStartItem;
+    DiagramItem *myEndItem;
+    QPolygonF    arrowHead;
+    QColor       myColor = Qt::black;
+};
+
+//! [0]
+
+#endif // ARROW_H
